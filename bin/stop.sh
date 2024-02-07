3611fec6671258c860999242f2d9b4255e7fdbc5
haoteW <htwang2022@163.com>
2024-02-08 05:10:52 +0800

    fix script

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 杀死具体的 Go 进程，根据程序名称或其他标识符来匹配
-function stop_program {
+stop_program() {
     program_name="$1"
-    process_id=$(pgrep -f "$program_name")
+    process_id=$(pgrep -o -f "$program_name")
 
     if [ -n "$process_id" ]; then
         echo "Stopping $program_name (PID: $process_id)..."
@@ -16,8 +16,8 @@ function stop_program {
 # 停止 main.go
 stop_program "main.go"
 
-# 停止 start.go
-stop_program "start.go"
+# 停止 starter.go
+stop_program "starter"
 
 # 停止 frontend.go
-stop_program "frontend.go"
+stop_program "frontend"
